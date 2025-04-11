6dc0532c84e930add4552239ecd28f59d4a2bae6
jpPinheiro03 <joao.pinheiro@sptech.school>
2025-04-11 11:37:55 -0300

    Atualizando API insert

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -22,7 +22,7 @@ const serial = async (
             host: '127.0.0.1',
             user: 'aluno',
             password: 'Sptech#2024',
-            database: 'OilSense',
+            database: 'Oilsense',
             port: 3307
         }
     ).promise();
@@ -63,7 +63,7 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO Telemetria (alturaOleo, dtHoraColeta) VALUES (?, CURRENT_TIMESTAMP())',
+                'INSERT INTO telemetria (alturaoleo, dtColeta, fksensor) VALUES (?, CURRENT_TIMESTAMP(), 100)',
                 [sensorUltrassonico]
             );
             console.log("valores inseridos no banco: ", sensorUltrassonico);
